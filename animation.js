ead06b53a4bccc79bde3db1aa2f49ebb38d6deab
gproduct <gproduct1379@yahoo.com>
2015-06-12 23:09:22 +0200

    first commit

---
+++ animation.js
@@ -0,0 +1,36 @@
+			$("#facebookLeft").hover(function(){
+				$(this).animate({width: "100"}).clearQueue();
+				$("#facebookLogo").animate({marginLeft:"30px"}).clearQueue();
+			},
+			function(){
+				$(this).animate({width: "60"}).clearQueue();
+				$("#facebookLogo").animate({marginLeft:"0px"}).clearQueue();			
+			});
+			$("#googleplusLeft").hover(function(){
+				$(this).animate({width: "100"}).clearQueue();
+				$("#googleplusLogo").animate({marginLeft:"30px"}).clearQueue();
+			},
+			function(){
+				$(this).animate({width: "60"}).clearQueue();
+				$("#googleplusLogo").animate({marginLeft:"0px"}).clearQueue();			
+			});
+			$("#twitterLeft").hover(function(){
+				$(this).animate({width: "100"}).clearQueue();
+				$("#twitterLogo").animate({marginLeft:"30px"}).clearQueue();
+			},
+			function(){
+				$(this).animate({width: "60"}).clearQueue();
+				$("#twitterLogo").animate({marginLeft:"0px"}).clearQueue();			
+			});
+			$("#rssLeft").hover(function(){
+				$(this).animate({width: "100"}).clearQueue();
+				$("#rssLogo").animate({marginLeft:"30px"}).clearQueue();
+			},
+			function(){
+				$(this).animate({width: "60"}).clearQueue();	
+				$("#rssLogo").animate({marginLeft:"0px"}).clearQueue();		
+			});
+/************************************
+Copyright
+	Gal Silard | G-Product Serbia 2015
+*************************************/
